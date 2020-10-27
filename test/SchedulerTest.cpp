b79157752714e52f973911a1e41c978d712dd3b3
Martin Bakos <bakosmartin@gmail.com>
2020-10-27 18:05:53 +0100

    Create TYPED_TESTs to channel creation

--- test/SchedulerTest.cpp
+++ test/SchedulerTest.cpp
@@ -1,11 +1,33 @@
 #include "pipert/Scheduler.h"
 #include "gtest/gtest.h"
 
+class Human {
+public:
+  Human(const std::string& name, int age) : name_(name), age_(age) {}
+
+  std::string GetName() const { return name_; }
+  int GetAge() const { return age_; }
+
+private:
+  std::string name_;
+  int age_;
+};
+
 using SchedulerDeathTest = pipert::Scheduler;
+
+template <class T>
+class SchedulerTest : public testing::Test {};
+
+using MyTypes = ::testing::Types<char, int, Human>;
+TYPED_TEST_SUITE(SchedulerTest, MyTypes, );
 
 void SchedulerInitTestWithParameter(int workers) {
   pipert::Scheduler sch(workers);
   exit (0);
+}
+
+void DoNothing() {
+  return;
 }
 
 TEST(SchedulerDeathTest, InitializationWithParameter) {
@@ -36,4 +58,23 @@ TEST(SchedulerDeathTest, StopNotRunningScheduler) {
   pipert::Scheduler sch;
 
   EXPECT_DEBUG_DEATH(sch.Stop(), ".*");
+}
+
+TYPED_TEST(SchedulerTest, PolledChannelCreationWithTypes) {
+  pipert::Scheduler sch(0);
+  pipert::PolledChannel<TypeParam> pc =  sch.CreatePolledChannel<TypeParam>("PolledChannel", 1);
+
+  EXPECT_EQ(pc.GetName(), "PolledChannel");
+  EXPECT_EQ(pc.GetCapacity(), 1);
+  EXPECT_EQ(pc.GetPacketSize(), sizeof(pipert::Packet<TypeParam>));
+}
+
+TYPED_TEST(SchedulerTest, ScheduledChannelCreationWithTypes) {
+  pipert::Scheduler sch(0);
+  pipert::ScheduledChannel<TypeParam> sc = sch.CreateScheduledChannel<TypeParam>
+                                          ("ScheduledChannel", 1, nullptr, std::bind(&DoNothing));
+
+  EXPECT_EQ(sc.GetName(), "ScheduledChannel");
+  EXPECT_EQ(sc.GetCapacity(), 1);
+  EXPECT_EQ(sc.GetPacketSize(), sizeof(pipert::Packet<TypeParam>));
 }
