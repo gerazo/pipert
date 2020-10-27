ac2ea503bb3149f8093cad56e944815a1064f048
Martin Bakos <bakosmartin@gmail.com>
2020-10-27 18:04:51 +0100

    Add tests to Start and Stop methods

--- test/SchedulerTest.cpp
+++ test/SchedulerTest.cpp
@@ -22,3 +22,18 @@ TEST(SchedulerDeathTest, InitializationWithParameter) {
 TEST(SchedulerDeathTest, InitializationWithoutParameter) {
   EXPECT_EXIT({pipert::Scheduler sch; exit (0);}, ::testing::ExitedWithCode(0), ".*");
 }
+
+TEST(SchedulerDeathTest, SchedulerDoubleStart) {
+  testing::FLAGS_gtest_death_test_style="threadsafe";
+  pipert::Scheduler sch;
+
+  sch.Start();
+  EXPECT_DEBUG_DEATH(sch.Start(), ".*");
+  sch.Stop();
+}
+
+TEST(SchedulerDeathTest, StopNotRunningScheduler) {
+  pipert::Scheduler sch;
+
+  EXPECT_DEBUG_DEATH(sch.Stop(), ".*");
+}
