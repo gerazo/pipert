4c4629e5b8100f0e38baa74417c850fd2613abf5
Martin Bakos <bakosmartin@gmail.com>
2020-10-28 17:04:16 +0100

    Use Google's coding style in SchedulerTest file

--- test/SchedulerTest.cpp
+++ test/SchedulerTest.cpp
@@ -2,13 +2,13 @@
 #include "gtest/gtest.h"
 
 class Human {
-public:
+ public:
   Human(const std::string& name, int age) : name_(name), age_(age) {}
 
   std::string GetName() const { return name_; }
   int GetAge() const { return age_; }
 
-private:
+ private:
   std::string name_;
   int age_;
 };
@@ -23,30 +23,36 @@ TYPED_TEST_SUITE(SchedulerTest, MyTypes, );
 
 void SchedulerInitTestWithParameter(int workers) {
   pipert::Scheduler sch(workers);
-  exit (0);
+  exit(0);
 }
 
-void DoNothing() {
-  return;
-}
+void DoNothing() { return; }
 
 TEST(SchedulerDeathTest, InitializationWithParameter) {
 #ifdef DEBUG
   EXPECT_DEATH(SchedulerInitTestWithParameter(-5), ".*");
   EXPECT_DEATH(SchedulerInitTestWithParameter(-1), ".*");
-#endif //DEBUG
+#endif  // DEBUG
 
-  EXPECT_EXIT(SchedulerInitTestWithParameter(0), ::testing::ExitedWithCode(0), ".*");
-  EXPECT_EXIT(SchedulerInitTestWithParameter(1), ::testing::ExitedWithCode(0), ".*");
-  EXPECT_EXIT(SchedulerInitTestWithParameter(20), ::testing::ExitedWithCode(0), ".*");
+  EXPECT_EXIT(SchedulerInitTestWithParameter(0), ::testing::ExitedWithCode(0),
+              ".*");
+  EXPECT_EXIT(SchedulerInitTestWithParameter(1), ::testing::ExitedWithCode(0),
+              ".*");
+  EXPECT_EXIT(SchedulerInitTestWithParameter(20), ::testing::ExitedWithCode(0),
+              ".*");
 }
 
 TEST(SchedulerDeathTest, InitializationWithoutParameter) {
-  EXPECT_EXIT({pipert::Scheduler sch; exit (0);}, ::testing::ExitedWithCode(0), ".*");
+  EXPECT_EXIT(
+      {
+        pipert::Scheduler sch;
+        exit(0);
+      },
+      ::testing::ExitedWithCode(0), ".*");
 }
 
 TEST(SchedulerDeathTest, SchedulerDoubleStart) {
-  testing::FLAGS_gtest_death_test_style="threadsafe";
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
   pipert::Scheduler sch;
 
   sch.Start();
@@ -62,7 +68,8 @@ TEST(SchedulerDeathTest, StopNotRunningScheduler) {
 
 TYPED_TEST(SchedulerTest, PolledChannelCreationWithTypes) {
   pipert::Scheduler sch(0);
-  pipert::PolledChannel<TypeParam> pc =  sch.CreatePolledChannel<TypeParam>("PolledChannel", 1);
+  pipert::PolledChannel<TypeParam> pc =
+      sch.CreatePolledChannel<TypeParam>("PolledChannel", 1);
 
   EXPECT_EQ(pc.GetName(), "PolledChannel");
   EXPECT_EQ(pc.GetCapacity(), 1);
@@ -71,8 +78,9 @@ TYPED_TEST(SchedulerTest, PolledChannelCreationWithTypes) {
 
 TYPED_TEST(SchedulerTest, ScheduledChannelCreationWithTypes) {
   pipert::Scheduler sch(0);
-  pipert::ScheduledChannel<TypeParam> sc = sch.CreateScheduledChannel<TypeParam>
-                                          ("ScheduledChannel", 1, nullptr, std::bind(&DoNothing));
+  pipert::ScheduledChannel<TypeParam> sc =
+      sch.CreateScheduledChannel<TypeParam>("ScheduledChannel", 1, nullptr,
+                                            std::bind(&DoNothing));
 
   EXPECT_EQ(sc.GetName(), "ScheduledChannel");
   EXPECT_EQ(sc.GetCapacity(), 1);
