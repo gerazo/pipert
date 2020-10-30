af17864f706e7db4119eea3ba5fccca1e40d0714
Martin Bakos <bakosmartin@gmail.com>
2020-10-30 13:19:33 +0100

    Drop tests which test assertions in implemetation

--- test/SchedulerTest.cpp
+++ test/SchedulerTest.cpp
@@ -15,58 +15,13 @@ class Human {
   int age_;
 };
 
-using SchedulerDeathTest = pipert::Scheduler;
-
 template <class T>
 class SchedulerTest : public testing::Test {};
 
 using MyTypes = ::testing::Types<char, int, Human>;
 TYPED_TEST_SUITE(SchedulerTest, MyTypes, );
 
-void SchedulerInitTestWithParameter(int workers) {
-  pipert::Scheduler sch(workers);
-  exit(0);
-}
-
 void DoNothing() { return; }
-
-TEST(SchedulerDeathTest, InitializationWithParameter) {
-#ifndef NDEBUG
-  EXPECT_DEATH(SchedulerInitTestWithParameter(-5), ".*");
-  EXPECT_DEATH(SchedulerInitTestWithParameter(-1), ".*");
-#endif  // NDEBUG
-
-  EXPECT_EXIT(SchedulerInitTestWithParameter(0), ::testing::ExitedWithCode(0),
-              ".*");
-  EXPECT_EXIT(SchedulerInitTestWithParameter(1), ::testing::ExitedWithCode(0),
-              ".*");
-  EXPECT_EXIT(SchedulerInitTestWithParameter(20), ::testing::ExitedWithCode(0),
-              ".*");
-}
-
-TEST(SchedulerDeathTest, InitializationWithoutParameter) {
-  EXPECT_EXIT(
-      {
-        pipert::Scheduler sch;
-        exit(0);
-      },
-      ::testing::ExitedWithCode(0), ".*");
-}
-
-TEST(SchedulerDeathTest, SchedulerDoubleStart) {
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
-  pipert::Scheduler sch;
-
-  sch.Start();
-  EXPECT_DEBUG_DEATH(sch.Start(), ".*");
-  sch.Stop();
-}
-
-TEST(SchedulerDeathTest, StopNotRunningScheduler) {
-  pipert::Scheduler sch;
-
-  EXPECT_DEBUG_DEATH(sch.Stop(), ".*");
-}
 
 TYPED_TEST(SchedulerTest, PolledChannelCreationWithTypes) {
   pipert::Scheduler sch(0);
