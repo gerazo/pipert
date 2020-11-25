d52c0885cdcb1ed0b7c5cba54685939abe5ae099
Basil Kaikoni <baselqyqony@gmail.com>
2020-11-25 15:05:10 -0500

    add MeasurementProfileBase

---
+++ include/pipert/MeasurementProfileBase.h
@@ -0,0 +1,28 @@
+#ifndef _MeasurementProfileBase_H_
+#define _MeasurementProfileBase_H_
+
+namespace pipert {
+
+    // this enum define the last level of processing the packet reached in the
+// channel
+enum MeasurementProfileProcessStatus {
+  NoStatus,
+  Acquired,
+  Pushed,
+  Popped,
+  Executed,
+  Dropped
+};
+class MeasurementProfileBase {
+ public:
+  bool IsOptionalUserProfile;
+  MeasurementProfileProcessStatus processState; 
+
+  MeasurementProfileBase(bool isOptionalUserProfile) {
+    IsOptionalUserProfile = isOptionalUserProfile;
+  }
+
+  // must
+};
+}  // namespace pipert
+#endif  //_PMeasurementProfileBase_H_
