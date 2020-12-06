d28002503b83f7630a90fc590cc65979912145ae
Basil Kaikoni <baselqyqony@gmail.com>
2020-12-06 09:10:07 +0100

    move timestamps records from measurementprofile to MeasurementEvent

--- include/pipert/MeasurementProfile.h
+++ include/pipert/MeasurementProfile.h
@@ -1,25 +1,22 @@
+#ifndef _MeasurementProfile_H_
+#define _MeasurementProfile_H_
 #include "pipert/MeasurementProfileBase.h"
 #include "pipert/PacketBase.h"
 #include "pipert/Timer.h"
+#include "pipert/MeasurementEvent.h"
 namespace pipert {
 
 class MeasurementProfile : MeasurementProfileBase {
- private:
-  const char* MesurementProfileName;
-  const char* ChannelName;
-  Timer::Time PacketTimeStamp;
-  Timer::Time AcquireTime;
-  Timer::Time PushTime;
-  Timer::Time PopTime;
-  Timer::Time ExecutionStartTime;
-  Timer::Time DroppingTime;
-  MeasurementProfileProcessStatus ProcessStatus;
 
-  int* ThreadID;
+
 
  public:
+  const char* MesurementProfileName;
+  std::vector<MeasurementEvent> MeasurementsEventsLog;
   MeasurementProfile() : MeasurementProfileBase(false) {
-    ProcessStatus = NoStatus;
+
   }
 };
 }  // namespace pipert
+  // namespace pipert
+#endif  //_PMeasurementProfileBase_H_
