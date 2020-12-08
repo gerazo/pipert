292bf671db8d545a1f786206cdd640664d89df6c
Zoltan Gera <gerazo@caesar.elte.hu>
2020-12-08 18:12:16 +0100

    Untested data gathering logic.

--- src/ProfileData.cpp
+++ src/ProfileData.cpp
@@ -1,6 +1,7 @@
 #include "ProfileData.h"
 
 #include <cassert>
+#include <cstring>
 
 #include "pipert/LogEventTypeRegistrar.h"
 
@@ -16,10 +17,16 @@ ProfileData::ProfileData(const char* data_group_name)
     : data_group_name_(data_group_name), sender_logger_(kEventPushed) {
   // We should have at least the built-in types registered.
   assert(LogEventTypeRegistrar::GetRegisterLength() >= 5);
+
+  // prepare the hash to contain every possible event type
   for (int i = 0; i < LogEventTypeRegistrar::GetRegisterLength(); i++) {
     aggregates_[LogEventTypeRegistrar::GetLogEventTypeID(i)];
   }
+
+  serialize_next_ = aggregates_.end();
 }
+
+const char* ProfileData::GetName() const { return data_group_name_; }
 
 void ProfileData::Log(LogEventBase log_event) {
   auto it = aggregates_.find(log_event.GetName());
@@ -31,6 +38,41 @@ void ProfileData::Log(LogEventBase log_event) {
   sender_logger_.TrackSender(log_event.GetName());
 }
 
-const char* ProfileData::GetName() const { return data_group_name_; }
+bool ProfileData::GatherNSerialize(std::uint8_t* buffer,
+                                   std::uint8_t*& buffer_end) {
+  const int safety_limit = (buffer_end - buffer) - 64;
+  assert(safety_limit >= 64);
+  std::uint8_t* buf = buffer;
+  // compulsory header (max 32 bytes)
+  memcpy(buf, "DGRP", 4);
+  buf += 4;
+  buf = SerializeString(buf, data_group_name_);
+  assert(buf <= buffer_end);
+  if (serialize_next_ == aggregates_.end()) {
+    buf = sender_logger_.SerializeNClear(buf);
+    serialize_next_ = aggregates_.begin();
+  }
+  assert(buf <= buffer_end);
+  while (buf <= buffer + safety_limit && serialize_next_ != aggregates_.end()) {
+    memcpy(buf, "LOGA", 4);
+    buf += 4;
+    buf = SerializeString(buf, serialize_next_->first);
+    assert(buf <= buffer_end);
+    buf = serialize_next_->second.SerializeNClear(buf);
+    assert(buf <= buffer_end);
+    serialize_next_++;
+  }
+  buffer_end = buf;
+  return serialize_next_ != aggregates_.end();
+}
+
+std::uint8_t* ProfileData::SerializeString(std::uint8_t* buf, const char* str) {
+  int len = (int)strlen(str);
+  if (len > kMaxNameLength) len = kMaxNameLength;
+  memcpy(buf, str, len);
+  buf += len + 1;
+  buf[-1] = 0;
+  return buf;
+}
 
 }  // namespace pipert
--- src/ProfileData.h
+++ src/ProfileData.h
@@ -3,10 +3,11 @@
 
 #include "pipert/LogEventBase.h"
 
+#include <cstdint>
+#include <unordered_map>
+
 #include "LogAggregate.h"
 #include "SenderLogger.h"
-
-#include <unordered_map>
 
 namespace pipert {
 
@@ -18,18 +19,27 @@ class ProfileData {
   static const char kEventFillTime[];
   static const char kEventReadTime[];
 
+  /// Maximum length of all names (group and event) that will be serialized.
+  static const int kMaxNameLength = 27;
+
   ProfileData(const char* data_group_name);
+
+  const char* GetName() const;
 
   void Log(LogEventBase log_event);
 
-  const char* GetName() const;
+  bool GatherNSerialize(std::uint8_t* buffer, std::uint8_t*& buffer_end);
 
  private:
   using LogAggregateKey = const char*;
 
+  static std::uint8_t* SerializeString(std::uint8_t* buf, const char* str);
+
   const char* data_group_name_;
   std::unordered_map<LogAggregateKey, LogAggregate> aggregates_;
   SenderLogger sender_logger_;
+
+  std::unordered_map<LogAggregateKey, LogAggregate>::iterator serialize_next_;
 };
 
 }  // namespace pipert
