520f323f4dfe95439bc6670ba57755e84c45e246
ip <ivanyipatrik96@inf.elte.hu>
2020-12-13 19:20:11 +0100

    Change the test cases and include merge testing

--- test/StateSchedulingTest.cpp
+++ test/StateSchedulingTest.cpp
@@ -5,7 +5,7 @@
 #include "pipert/Timer.h"
 #include "gtest/gtest.h"
 #include <iostream>
-#include <unistd.h>
+#include <thread> 
 
 namespace {
 
@@ -28,35 +28,30 @@ class Processor : public Base {
   }
 };
 
-class Incrementor : public Base {
+class Merger : public Base {
  public:
-  Incrementor(::pipert::ScheduledChannel<int>&& decChannel) : decChannel_(std::move(decChannel)) {}
   void Increment(::pipert::PacketToProcess<int> packet) {
     value_ += packet.data();
-    ::pipert::PacketToFill<int> new_packet = decChannel_.Acquire("Decrementor", packet.timestamp(), packet.data());
-    while(new_packet.IsEmpty()) {
-      new_packet = decChannel_.Acquire("Decrementor", packet.timestamp(), packet.data());
-    }
-    new_packet.Push();
   }
 
-  int GetDroppedPacketsNumber() {
-    return decChannel_.GetDroppedPacketsNumber();
-  }
- private:
-  ::pipert::ScheduledChannel<int> decChannel_;
-};
-
-class Decrementor : public Base {
- public:
   void Decrement(::pipert::PacketToProcess<int> packet) {
     value_ -= packet.data();
   }
 };
 
+void GenerateDataToChannel(::pipert::ScheduledChannel<int>* channel, const char* name) {
+  for (int i = 0; i < 100; ++i) {
+    ::pipert::PacketToFill<int> packet = channel->Acquire(name, ::pipert::Timer::time(), 1);
+    while (packet.IsEmpty()) {
+      packet = channel->Acquire(name, ::pipert::Timer::time(), 1);
+    }
+    packet.Push();
+  }
+}
+
 }  // namespace
 
-TEST(StateScheduling, SchedulingWithoutState) {
+TEST(StateScheduling, SchedulingWithoutStateTest) {
   ::pipert::Scheduler sch(4);
   Processor processor;
   ::pipert::ScheduledChannel<int> incrementer = sch.CreateScheduledChannel<int>("Incrementer", 4, nullptr, std::bind(&Processor::Process, &processor, std::placeholders::_1));
@@ -72,7 +67,7 @@ TEST(StateScheduling, SchedulingWithoutState) {
   EXPECT_TRUE(-1000 <= processor.GetValue() && processor.GetValue() <= 1000);
 }
 
-TEST(StateScheduling, SchedulingWithState) {
+TEST(StateScheduling, SchedulingWithStateTest) {
   ::pipert::Scheduler sch(4);
   Processor processor;
   ::pipert::ScheduledChannel<int> incrementer = sch.CreateScheduledChannel<int>("Processor", 4, &processor, std::bind(&Processor::Process, &processor, std::placeholders::_1));
@@ -86,40 +81,32 @@ TEST(StateScheduling, SchedulingWithState) {
   EXPECT_EQ(processor.GetValue(), 0);
 }
 
-TEST(StateScheduling, SchedulingDropCheck) {
+TEST(StateScheduling, SchedulingDropCheckTest) {
   ::pipert::Scheduler sch(4);
-  Decrementor decrementor;
-  ::pipert::ScheduledChannel<int> decChannel = sch.CreateScheduledChannel<int>("Decrementor", 4, &decrementor, std::bind(&Decrementor::Decrement, &decrementor, std::placeholders::_1));
+  Merger incrementor;
+  ::pipert::ScheduledChannel<int> incChannel 
+            = sch.CreateScheduledChannel<int>("Incrementor", 4, &incrementor, std::bind(&Merger::Increment, &incrementor, std::placeholders::_1));
   sch.Start();
-  for (int i = 0; i < 100; ++i) {
-    ::pipert::PacketToFill<int> packet = decChannel.Acquire("Decrementor", ::pipert::Timer::time(), 1);
-    while (packet.IsEmpty()) {
-      packet = decChannel.Acquire("Decrementor", ::pipert::Timer::time(), 1);
-    }
-    packet.Push();
-  }
-  usleep(1000);
-  sch.Stop();
-  EXPECT_EQ(decrementor.GetValue() - decChannel.GetDroppedPacketsNumber(), -100);
-}
-
-TEST(StateScheduling, SchedulingMultipleChannel) {
-  ::pipert::Scheduler sch(4);
-  Decrementor decrementor;
-  Incrementor incrementor(sch.CreateScheduledChannel<int>("Decrementor", 4, &decrementor, std::bind(&Decrementor::Decrement, &decrementor, std::placeholders::_1)));
-  ::pipert::ScheduledChannel<int> incChannel = sch.CreateScheduledChannel<int>("Incrementor", 4, &incrementor, std::bind(&Incrementor::Increment, &incrementor, std::placeholders::_1));
-
-  sch.Start();
-  for (int i = 0; i < 100; ++i) {
-    ::pipert::PacketToFill<int> packet = incChannel.Acquire("Incrementor", ::pipert::Timer::time(), 1);
-    while (packet.IsEmpty()) {
-      packet = incChannel.Acquire("Incrementor", ::pipert::Timer::time(), 1);
-    }
-    packet.Push();
-  }
-   
-  usleep(1000);
+  std::thread incThread(GenerateDataToChannel, &incChannel, "Incrementor");
+  incThread.join();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
   sch.Stop();
   EXPECT_EQ(incrementor.GetValue() + incChannel.GetDroppedPacketsNumber(), 100);
-  EXPECT_EQ(decrementor.GetValue() - incrementor.GetDroppedPacketsNumber() - incChannel.GetDroppedPacketsNumber(), -100);
+}
+
+TEST(StateScheduling, MergeSchedulingTest) {
+  ::pipert::Scheduler sch(4);
+  Merger merger;
+  ::pipert::ScheduledChannel<int> incChannel = sch.CreateScheduledChannel<int>("Incrementor", 4, &merger, std::bind(&Merger::Increment, &merger, std::placeholders::_1));
+  ::pipert::ScheduledChannel<int> decChannel = sch.CreateScheduledChannel<int>("Decrementor", 4, &merger, std::bind(&Merger::Decrement, &merger, std::placeholders::_1));
+  sch.Start();
+  std::thread incThread(GenerateDataToChannel, &incChannel, "Incrementor");
+  std::thread decThread(GenerateDataToChannel, &decChannel, "Decrementor");
+  
+  incThread.join();
+  decThread.join();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  sch.Stop();
+
+  EXPECT_EQ(merger.GetValue() + incChannel.GetDroppedPacketsNumber() - decChannel.GetDroppedPacketsNumber(), 0);
 }
