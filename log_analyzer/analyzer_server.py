ef0b10eee0ebf15799a98615d40ecdfc4a406b50
Hossam <hossamabdeen17@gmail.com>
2021-01-25 20:15:45 +0100

    basic server

---
+++ log_analyzer/analyzer_server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+class AnalyzerServer(object):
+    def __init__(self, ip, port):
+        self.__ip = ip
+        self.__port = port
+        self.__output = ""
+        self.__close = False
+
+    def __configure_server(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.__ip, self.__port))
+        return sock
+
+    def __start_server(self):
+        s = self.__configure_server()
+        while True:
+            data, address = s.recvfrom(1024)
+            self.__output += data.decode()
+
+    def run(self):
+        self.__configure_server()
+        self.__start_server()
+
+    def get_output(self):
+        return self.__output
