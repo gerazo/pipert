56f7dd16f4bc5df9bffe9a9b537a4fb0110d9c2e
Hossam <hossamabdeen17@gmail.com>
2021-02-17 20:12:15 +0100

    seperating the profiling pipeline testing from scheduler

--- test/SchedulerNProfilerTest.cpp
+++ test/SchedulerNProfilerTest.cpp
@@ -51,66 +51,10 @@ class HumanPrinter {
  private:
   pipert::ScheduledChannel<Human>* ch_to_write_;
 };
-
-template <class T>
-class SchedulerTest : public testing::Test {};
-
-using MyTypes = ::testing::Types<char, int, Human>;
-TYPED_TEST_SUITE(SchedulerTest, MyTypes, );
-
-void DoNothing() { return; }
-
 }  // namespace
 
-TEST(Scheduler, SchedulerInitializationTest) {
-  pipert::Scheduler sch0;
-  pipert::Scheduler sch1(-1);
-  pipert::Scheduler sch2(0);
-  pipert::Scheduler sch3(1);
-
-  EXPECT_EQ(sch0.GetWorkerNumber(), std::thread::hardware_concurrency());
-  EXPECT_EQ(sch1.GetWorkerNumber(), std::thread::hardware_concurrency());
-  EXPECT_EQ(sch2.GetWorkerNumber(), std::thread::hardware_concurrency());
-  EXPECT_EQ(sch3.GetWorkerNumber(), 1);
-}
-
-TEST(Scheduler, StartStopRestartTest) {
-  pipert::Scheduler sch;
-
-  EXPECT_FALSE(sch.IsRunning());
-  sch.Start();
-  EXPECT_TRUE(sch.IsRunning());
-  sch.Stop();
-  EXPECT_FALSE(sch.IsRunning());
-  sch.Start();
-  EXPECT_TRUE(sch.IsRunning());
-  sch.Stop();
-  EXPECT_FALSE(sch.IsRunning());
-}
-
-TYPED_TEST(SchedulerTest, PolledChannelCreationWithTypes) {
-  pipert::Scheduler sch(0);
-  pipert::PolledChannel<TypeParam> pc =
-      sch.CreatePolledChannel<TypeParam>("PolledChannel", 1);
-
-  EXPECT_EQ(pc.GetName(), "PolledChannel");
-  EXPECT_EQ(pc.GetCapacity(), 1);
-  EXPECT_EQ(pc.GetPacketSize(), sizeof(pipert::Packet<TypeParam>));
-}
-
-TYPED_TEST(SchedulerTest, ScheduledChannelCreationWithTypes) {
-  pipert::Scheduler sch(0);
-  pipert::ScheduledChannel<TypeParam> sc =
-      sch.CreateScheduledChannel<TypeParam>("ScheduledChannel", 1, nullptr,
-                                            std::bind(&DoNothing));
-
-  EXPECT_EQ(sc.GetName(), "ScheduledChannel");
-  EXPECT_EQ(sc.GetCapacity(), 1);
-  EXPECT_EQ(sc.GetPacketSize(), sizeof(pipert::Packet<TypeParam>));
-}
-
-TEST(Scheduler, SchedulerPipeLineTest) {
-  pipert::Scheduler sch;
+TEST(Scheduler, SchedulerProfilerPipeLineTest) {
+  pipert::Scheduler sch(0, pipert::Profiler("file:profilerlog.txt"));
   int channel_capacity = 10;
   pipert::PolledChannel<Human> pc =
       sch.CreatePolledChannel<Human>("OutChannel", channel_capacity);
@@ -135,6 +79,10 @@ TEST(Scheduler, SchedulerPipeLineTest) {
   while(pc.GetQueuedBufferLength() != channel_capacity)
     std::this_thread::yield();
 
+  sch.GetProfiler().GatherNSend();
+  EXPECT_EQ(sch.GetProfiler().GetAggregationTime(), 0);
+  EXPECT_EQ(sch.GetProfiler().GetBufferSize(), 4096);
+  EXPECT_EQ(std::remove("profilerlog.txt"), 0);
   sch.Stop();
 
   for(pipert::PacketToProcess<Human> packet_to_process = pc.Poll();
