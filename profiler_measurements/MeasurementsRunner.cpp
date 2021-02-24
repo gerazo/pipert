2502acc48382baa60357ac202068e3fc359197bb
baselqyqony <baselqyqony@gmail.com>
2021-02-24 22:03:57 +0100

    apply clang format on MeasurementsRunner.cpp and longDelayChannelScenario.h

--- profiler_measurements/MeasurementsRunner.cpp
+++ profiler_measurements/MeasurementsRunner.cpp
@@ -1,23 +1,11 @@
-#include "pipert/Scheduler.h"
-#include "pipert/Profiler.h"
-#include <thread>
-#include <string>
-#include <algorithm>
-#include<iostream>
-#include <unistd.h>
-#include <termio.h>
-#include <fcntl.h>
 #include "longDelayChannelScenario.h"
-
-
 namespace measurements {
 
+int main() {
+  longDelayChannelScenario long_delay_scenario;
+  // press any key while running to stop streaming
+  long_delay_scenario.run("UDP:127.0.0.1:888");
 
-    int main() {
-        longDelayChannelScenario long_delay_scenario;
-        //press any key while running to stop streaming
-        long_delay_scenario.run("UDP:127.0.0.1:888");
-
-        return 0;
-    }
+  return 0;
 }
+}  // namespace measurements
--- profiler_measurements/longDelayChannelScenario.h
+++ profiler_measurements/longDelayChannelScenario.h
@@ -1,139 +1,142 @@
 #ifndef PIPERT_LONGDELAYCHANNELSCENARIO_H
 #define PIPERT_LONGDELAYCHANNELSCENARIO_H
-#include "pipert/Scheduler.h"
-#include "pipert/Profiler.h"
-#include <thread>
-#include <string>
-#include <algorithm>
-#include<iostream>
-#include <unistd.h>
-#include <termio.h>
 #include <fcntl.h>
+#include <termio.h>
+#include <unistd.h>
 
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include "pipert/Profiler.h"
+#include "pipert/Scheduler.h"
 
 /// Measurement Title : Long Delay Channel detection
 /// in this measurement we will generate three channels one of them have
-/// relatively longer processing time compared to the average processing  time of the other channels
+/// relatively longer processing time compared to the average processing  time
+/// of the other channels
+///
+////Expected analysis output
+/// log analyser is Expected to activate this flag for the slow channel
+// analysis logic is to to compute the average time of channels and activate this flag for the channel which is having noticable higher value
 
 namespace measurements {
-    class Human {
-    public:
-        Human(const std::string &name, int age) : name_(name), age_(age) {}
+class Human {
+ public:
+  Human(const std::string &name, int age) : name_(name), age_(age) {}
 
-        std::string GetName() const { return name_; }
+  std::string GetName() const { return name_; }
 
-        int GetAge() const { return age_; }
+  int GetAge() const { return age_; }
 
-    private:
-        std::string name_;
-        int age_;
-    };
+ private:
+  std::string name_;
+  int age_;
+};
 
-    class HumanReverser {
-    public:
-        HumanReverser(pipert::PolledChannel<Human> *pc_to_write) : pc_to_write_(pc_to_write) {}
+class HumanReverser {
+ public:
+  HumanReverser(pipert::PolledChannel<Human> *pc_to_write)
+      : pc_to_write_(pc_to_write) {}
 
-        void ReverseName(pipert::PacketToProcess<Human> p) {
-            std::string name(p.data().GetName());
-            std::reverse(name.begin(), name.end());
-            pc_to_write_->Acquire(p.timestamp() + 10, name, p.data().GetAge());
-        }
+  void ReverseName(pipert::PacketToProcess<Human> p) {
+    std::string name(p.data().GetName());
+    std::reverse(name.begin(), name.end());
+    pc_to_write_->Acquire(p.timestamp() + 10, name, p.data().GetAge());
+  }
 
-    private:
-        pipert::PolledChannel<Human> *pc_to_write_;
-    };
+ private:
+  pipert::PolledChannel<Human> *pc_to_write_;
+};
 
+class HumanPrinter {
+ public:
+  void doNothing() {
+    int i = rand() % 99999900 + 1000;
 
-
-    class HumanPrinter {
-    public:
-        void doNothing() {
-            int i = rand() % 99999900 +1000;
-
-            while (i > 0) {
-                i--;
-            }
-        }
-
-        HumanPrinter(pipert::ScheduledChannel<Human> *ch_to_write) : ch_to_write_(ch_to_write) {}
-
-        void PrintName(pipert::PacketToProcess<Human> p) {
-            doNothing();
-            ch_to_write_->Acquire(p.timestamp() + 5, p.data());
-        }
-    private:
-        pipert::ScheduledChannel<Human> *ch_to_write_;
-
-    };
-
-
-
-
-}
-int kbhit(void)
-{
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if(ch != EOF)
-    {
-        ungetc(ch, stdin);
-        return 1;
+    while (i > 0) {
+      i--;
     }
+  }
 
-    return 0;
+  HumanPrinter(pipert::ScheduledChannel<Human> *ch_to_write)
+      : ch_to_write_(ch_to_write) {}
+
+  void PrintName(pipert::PacketToProcess<Human> p) {
+    doNothing();
+    ch_to_write_->Acquire(p.timestamp() + 5, p.data());
+  }
+
+ private:
+  pipert::ScheduledChannel<Human> *ch_to_write_;
+};
+
+}  // namespace measurements
+int kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
 }
 class longDelayChannelScenario {
+ public:
+  longDelayChannelScenario() {}
 
-public:
-    longDelayChannelScenario() {}
+  void run(const char *destination_uri) {
+    pipert::Scheduler sch(0, pipert::Profiler(destination_uri));
+    while (!kbhit()) {
+      int channel_capacity = 10;
+      pipert::PolledChannel<measurements::Human> pc =
+          sch.CreatePolledChannel<measurements::Human>("OutChannel",
+                                                       channel_capacity);
 
-    void run(const char *destination_uri)  {
-        pipert::Scheduler sch(0, pipert::Profiler(destination_uri));
-        while(!kbhit()) {
+      measurements::HumanReverser hr(&pc);
+      pipert::ScheduledChannel<measurements::Human> sc2 =
+          sch.CreateScheduledChannel<measurements::Human>(
+              "ReverserChannel", channel_capacity, nullptr,
+              std::bind(&measurements::HumanReverser::ReverseName, &hr,
+                        std::placeholders::_1));
 
-            int channel_capacity = 10;
-            pipert::PolledChannel<measurements::Human> pc =
-                    sch.CreatePolledChannel<measurements::Human>("OutChannel", channel_capacity);
+      measurements::HumanPrinter hp(&sc2);
+      pipert::ScheduledChannel<measurements::Human> sc1 =
+          sch.CreateScheduledChannel<measurements::Human>(
+              "PrinterChannel", channel_capacity, nullptr,
+              std::bind(&measurements::HumanPrinter::PrintName, &hp,
+                        std::placeholders::_1));
 
-            measurements::HumanReverser hr(&pc);
-            pipert::ScheduledChannel<measurements::Human> sc2 =
-                    sch.CreateScheduledChannel<measurements::Human>("ReverserChannel", channel_capacity, nullptr,
-                                                                    std::bind(&measurements::HumanReverser::ReverseName,
-                                                                              &hr, std::placeholders::_1));
+      sch.Start();
 
-            measurements::HumanPrinter hp(&sc2);
-            pipert::ScheduledChannel<measurements::Human> sc1 =
-                    sch.CreateScheduledChannel<measurements::Human>("PrinterChannel", channel_capacity, nullptr,
-                                                                    std::bind(&measurements::HumanPrinter::PrintName, &hp,
-                                                                              std::placeholders::_1));
+      pipert::Timer::Time time = pipert::Timer::time();
+      for (int i = 0; i < 10; i++) {
+        pipert::PacketToFill<measurements::Human> packet_to_fill =
+            sc1.Acquire(time, "Jimi Hendrix", 28 + i);
+      }
 
-            sch.Start();
-
-            pipert::Timer::Time time = pipert::Timer::time();
-            for (int i = 0; i < 10; i++) {
-                pipert::PacketToFill<measurements::Human> packet_to_fill = sc1.Acquire(time, "Jimi Hendrix", 28 + i);
-            }
-
-            while (pc.GetQueuedBufferLength() != channel_capacity)
-                std::this_thread::yield();
-            sch.GetProfiler().GatherNSend();
-            sch.Stop();
-        }
-
+      while (pc.GetQueuedBufferLength() != channel_capacity)
+        std::this_thread::yield();
+      sch.GetProfiler().GatherNSend();
+      sch.Stop();
     }
+  }
 };
-#endif //PIPERT_LONGDELAYCHANNELSCENARIO_H
+#endif  // PIPERT_LONGDELAYCHANNELSCENARIO_H
+
