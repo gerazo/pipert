11e7b16b9095ae20527e8562e3cd7d05ff9d6a6a
baselqyqony <baselqyqony@gmail.com>
2021-03-04 11:31:26 +0100

    add measurements scenario generation utils

---
+++ profiler_measurements/utils.h
@@ -0,0 +1,33 @@
+#ifndef TESTLIBRARY_UTILS_H
+#define TESTLIBRARY_UTILS_H
+
+///measurements scenario generation shared utils
+namespace measurements {
+
+    int kbhit(void) {
+        struct termios oldt, newt;
+        int ch;
+        int oldf;
+
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+        ch = getchar();
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+        if (ch != EOF) {
+            ungetc(ch, stdin);
+            return 1;
+        }
+
+        return 0;
+    }
+}
+
+#endif //TESTLIBRARY_UTILS_H
