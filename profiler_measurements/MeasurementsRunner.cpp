049f236a9f5e5354a61918cebbb5902f739e574e
baselqyqony <baselqyqony@gmail.com>
2021-03-14 19:18:26 +0100

    change targer measurements port to 8000

--- profiler_measurements/MeasurementsRunner.cpp
+++ profiler_measurements/MeasurementsRunner.cpp
@@ -7,19 +7,19 @@
 void generateChannelFreezeScenario( ){
     /// press any key to abort
     measurements::channelFreezeScenario channel_freeze_scenario_;
-    channel_freeze_scenario_.run("udp:127.0.0.1:8888");
+    channel_freeze_scenario_.run("udp:127.0.0.1:8000");
 }
 
 void generateLongDelayScenario( ){
     /// press any key to abort
     measurements::longDelayChannelScenario long_delay_scenario_;
-    long_delay_scenario_.run("udp:127.0.0.1:8888");
+    long_delay_scenario_.run("udp:127.0.0.1:8000");
 }
 
 void generateDroppingRateScenario( ){
     /// press any key to abort
     measurements::droppingRateScenario dropping_rate_scenatios ;
-    dropping_rate_scenatios.run("udp:127.0.0.1:8888");
+    dropping_rate_scenatios.run("udp:127.0.0.1:8000");
 }
 
 int main(){
