77dd07fdae07d9a1e745005e2bac5a3658bc4850
baselqyqony <baselqyqony@gmail.com>
2021-03-18 23:16:54 +0100

    add events getters functiontions drop_rate_calculator function ,add HIGHDROPRATE flag

--- log_analyzer/channel.py
+++ log_analyzer/channel.py
@@ -1,4 +1,5 @@
 FROZEN = "FROZEN"
+HIGHDROPRATE="HIGHDROPRATE"
 PACKETS_THRESHOULD = 20
 
 
@@ -6,7 +7,7 @@ class Channel(object):
     def __init__(self, name, events, latest_packet_id):
         self.__name = name
         self.__events = [events]
-        self.__flags = {FROZEN: False}
+        self.__flags = {FROZEN: False,HIGHDROPRATE:False}
         self.__packet_count = 1
         self.__latest_packet_id = latest_packet_id
 
@@ -42,6 +43,47 @@ class Channel(object):
 
     def set_latest_packet_id(self, latest_packet_id):
         self.__latest_packet_id = latest_packet_id
+    # return the flag if the dropping rate is high
+    # it will return -1 if the channel if both number of executed and dropped events are 0
+    # if there is no execution only dropping it will return 1
+    def drop_rate_calculator(self):
+        result=-2
+        number_of_dropped_events=len(self.get_drop_events())
+        number_of_executed_events=len(self.get_execution_events())
+        if(number_of_dropped_events==0 and number_of_executed_events==0):
+            result=-1
+        else:
+            if(number_of_executed_events==0):
+                result=1
+            else:
+                if(number_of_dropped_events==0):
+                    result=0
+                else:
+                    result=number_of_dropped_events/number_of_executed_events
+
+        return result
+    def get_fill_events(self):
+        return list(filter(lambda x: x.get_type()== "Fill Time", self.__flatten_list(self.get_events())))
+
+    def get_pushed_events(self):
+        return list(filter(lambda x: x.get_type()== "Packet Pushed", self.__flatten_list(self.get_events())))
+
+    def get_retrieved_events(self):
+        return list(filter(lambda x: x.get_type()== "Packet Retrieved", self.__flatten_list(self.get_events())))
+
+    def get_execution_events(self):
+        return list(filter(lambda x: x.get_type()== "Execution Time", self.__flatten_list(self.get_events())))
+
+    def get_read_events(self):
+        return list(filter(lambda x: x.get_type()== "Read Time", self.__flatten_list(self.get_events())))
+
+    def get_drop_events(self):
+        return list(filter(lambda x: x.get_type()== "Packet Dropped", self.__flatten_list(self.get_events())))
+
+
+
+    def __flatten_list(self, channels_list):
+        return [item for sublist in channels_list for item in sublist]
 
     def get_dict(self):
         dict = {
