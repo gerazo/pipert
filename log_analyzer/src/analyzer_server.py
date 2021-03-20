8e4019ee2d2852c5b8ee22a88232ae8adcdfe3e4
Hossam <hossamabdeen17@gmail.com>
2021-03-20 11:14:11 +0100

    Fixing the OSError

--- log_analyzer/src/analyzer_server.py
+++ log_analyzer/src/analyzer_server.py
@@ -14,6 +14,7 @@ class AnalyzerServer(object):
 
     def __configure_server(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((self.__ip, self.__port))
         return sock
 
