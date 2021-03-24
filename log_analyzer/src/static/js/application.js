c743d6eef074a75dc61afb152e43d115b81a1286
Hossam <hossamabdeen17@gmail.com>
2021-03-24 17:49:11 +0100

    WIP: Dynamic creating for the UI

--- log_analyzer/src/static/js/application.js
+++ log_analyzer/src/static/js/application.js
@@ -1,62 +1,40 @@
-let channels = [];
-
-// function does_exist(channel_name){
-//     const channel = channels.find(channel => channel.name === channel_name)
-//     if (channel === undefined){
-//         return false
-//     }
-
-//     return true
-// }
-
-// function parse_channel(packet){
-//     if (!(does_exist(packet.receiver_channel))){
-//         var channel_1 = {
-//             "name": packet.receiver_channel,
-//             "events": packet.events
-//         };
-
-//         channels.push(channel_1);
-//     }
-
-//    if (!(does_exist(packet.sender_channel))){
-//         var channel_1 = {
-//             "name": packet.sender_channel,
-//             "events": []
-//         };
-
-//         channels.push(channel_1);
-//     }
-
-// }
-
 $(document).ready(function(){
-
     var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-//    socket.on('connect', function() {
-//        socket.send("Connection established")
-//    });
-//
-//    socket.emit('event', 'Woahhh')
-//
-//    socket.on('message', function(msg){
-//        alert(msg)
-//    })
 
     socket.on('update_channels', function(channels){
         // parse_channel(packet);
         $(".channels-container").empty();
         $.each(channels, function(i, channel){
-            $(".channels-container").append("<h1>"+channel.name+"</h1>");
-            $(".channels-container").append("<h2>"+channel.flags.FROZEN+"</h2>");
+            const channel_div = create_channel(channel);
+            $(".channels-container").append(channel_div);
         });
-//        console.log(packet);
-//        $("body").append("<h1>Packet:-<h1>");
-//        $("body").append("<h2>"+packet.receiver_channel+"<h2>");
-//        $("body").append("<h2>"+packet.sender_channel+"<h2>");
-//        $.each(packet.events, function(i, event){
-//            $("body").append("<h3>"+event.type   +"<h3>");
-//        });
     });
 });
+
+function create_channel(channel){
+    const channel_div = document.createElement("div");
+    const name = create_element_with_text("H2", channel.name);
+    channel_div.appendChild(name);
+    create_flags(channel, channel_div);
+    channel_div.className = "channel col-3";
+
+    return channel_div
+}
+
+function create_element_with_text(elem, text){
+    const e = document.createElement(elem);
+    const t = document.createTextNode(text);
+    e.appendChild(t);
+    return e
+}
+
+function create_flags(channel, channel_div){
+    for (const [key, value] of Object.entries(channel.flags)) {
+        create_flag(channel_div, key, value);
+    }
+}
+
+function create_flag(channel_div, key, val){
+    const flag = create_element_with_text("P1", `${key}: ${val}`);
+    channel_div.appendChild(flag);
+}
