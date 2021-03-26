71b3ba17c1e494ea68f54b777a98595005902841
Martin Bakos <bakosmartin@gmail.com>
2021-03-26 22:22:58 +0100

    UDPConnection: extended UDPConnection's functionality

---
+++ include/pipert/UDPConnection.h
@@ -0,0 +1,47 @@
+#ifndef _UDPCONNECTION_H_
+#define _UDPCONNECTION_H_
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+namespace pipert {
+
+/// This class is responsible for UDP communication.
+class UDPConnection {
+ public:
+  /// Constructs new UDP connection instance.
+  ///
+  /// \param remote_port UDP connection port
+  /// \param remote_address UDP connection ip address
+  UDPConnection(int remote_port, const char* remote_address);
+
+  /// Constructs new UDP connection instance.
+  ///
+  /// \param binding_port UDP connection port
+  UDPConnection(int binding_port);
+
+  /// Destructor of UDPConnection.
+  ~UDPConnection();
+
+  /// Returns whether the UDP connection was properly initialized.
+  bool IsConnected() const { return socket_filedesc_ != -1; }
+
+  /// Sends the data through UDP connection.
+  /// \param buffer holds the buffer that needs to be sent.
+  /// \param size holds the buffer size.
+  void Send(void* buffer, int size);
+
+  /// Receives data from UDP connectom.
+  /// \param buffer provides the buffer in which to receive data.
+  /// \param size holds the buffer size.
+  /// \return The number of bytes received, or -1 if an error occurred.
+  int Receive(void* buffer, int size);
+
+ private:
+  int socket_filedesc_;
+  struct sockaddr_in local_address_, remote_address_;
+};
+
+}  // namespace pipert
+
+#endif  // end of _UDPCONNECTION_H
--- src/UDPConnection.cpp
+++ src/UDPConnection.cpp
@@ -1,4 +1,4 @@
-#include "UDPConnection.h"
+#include "pipert/UDPConnection.h"
 
 #include <cassert>
 #include <cstdint>
@@ -9,20 +9,39 @@
 #include <unistd.h>
 
 namespace pipert {
-
-UDPConnection::UDPConnection(int port, const char* address) {
+UDPConnection::UDPConnection(int remote_port, const char* remote_address) {
   socket_filedesc_ = socket(AF_INET, SOCK_DGRAM, 0);
   if (socket_filedesc_ != -1) {
-    memset(&server_address_, 0, sizeof(server_address_));
+    memset(&remote_address_, 0, sizeof(remote_address_));
     /// Filling server information
-    server_address_.sin_family = AF_INET;
-    server_address_.sin_port = htons((std::uint16_t)port);
-    int ok = inet_aton(address, &server_address_.sin_addr);
+    remote_address_.sin_family = AF_INET;
+    remote_address_.sin_port = htons((std::uint16_t)remote_port);
+    int ok = inet_aton(remote_address, &remote_address_.sin_addr);
     if (!ok) {
       assert(ok == 1);  // getting address failed
       socket_filedesc_ = -1;
       return;
     }
+  }
+  assert(socket_filedesc_ != -1);  // opening socket failed
+}
+
+UDPConnection::UDPConnection(int binding_port) {
+  socket_filedesc_ = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socket_filedesc_ != -1) {
+    memset(&remote_address_, 0, sizeof(remote_address_));
+    memset(&local_address_, 0, sizeof(local_address_));
+    local_address_.sin_family = AF_INET;
+    local_address_.sin_addr.s_addr = INADDR_ANY;
+	  local_address_.sin_port = htons((std::uint16_t)binding_port);
+    /// Bind the socket with the local address
+    int ok = bind(socket_filedesc_, (const struct sockaddr *)&local_address_,
+			        sizeof(local_address_));
+    if (ok) {
+      assert(ok == 1);  // binding failed
+      socket_filedesc_ = -1;
+      return;
+	  }
   }
   assert(socket_filedesc_ != -1);  // opening socket failed
 }
@@ -37,8 +56,18 @@ void UDPConnection::Send(void* buffer, int size) {
   assert(size > 0 && size <= 508);
   if (IsConnected()) {
     sendto(socket_filedesc_, buffer, size, 0,
-           (struct sockaddr*)&server_address_, sizeof(server_address_));
+           (struct sockaddr*)&remote_address_, sizeof(remote_address_));
   }
+}
+
+int UDPConnection::Receive(void* buffer, int size) {
+  assert(size > 0 && size <= 508);
+  if (IsConnected()) {
+    socklen_t len = sizeof(remote_address_);
+    return recvfrom(socket_filedesc_, buffer, size, 0,
+                    (struct sockaddr *)&remote_address_, &len);
+  }
+  return -1;
 }
 
 }  // namespace pipert
