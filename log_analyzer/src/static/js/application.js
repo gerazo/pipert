fcfd8fba078f2fdc11dbd5256989381e8bdbda9d
Hossam <hossamabdeen17@gmail.com>
2021-03-30 09:54:15 +0200

    Adding chart.js

--- log_analyzer/src/static/js/application.js
+++ log_analyzer/src/static/js/application.js
@@ -43,3 +43,39 @@ function create_flag(channel_div, key, val){
     const flag = create_element_with_text("P", text);
     channel_div.appendChild(flag);
 }
+
+
+$(document).ready(function(){
+    let myChart = document.getElementById('myChart').getContext('2d');
+
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.defaultFontColor = '#777';
+
+    let massPopChart = new Chart(myChart, {
+        type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data:{
+          labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge'],
+          datasets:[{
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162
+            ],
+            //backgroundColor:'green',
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }]
+        }
+      });
+});
