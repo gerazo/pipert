49f083d5bc113cc18660c7454e2b27235f31fad0
baselqyqony <baselqyqony@gmail.com>
2021-04-06 20:43:49 +0200

    add read time checker

---
+++ log_analyzer/src/checkers/read_time_checker.py
@@ -0,0 +1,13 @@
+from src.checkers.base_checker import BaseChecker
+from src.channel_manager import ChannelManager
+from src.constants import CHANNEL_READ_THRESHOLD,HIGH_READ_TIME
+
+
+class ReadTimeChecker(BaseChecker):
+    def run(self):
+        for channel in ChannelManager().get_channels():
+            if (ChannelManager().calculate_channel_time_to_read(channel) >
+               self._config[CHANNEL_READ_THRESHOLD]):
+                channel.update_flag(HIGH_READ_TIME, True)
+            else:
+                channel.update_flag(HIGH_READ_TIME, False)
