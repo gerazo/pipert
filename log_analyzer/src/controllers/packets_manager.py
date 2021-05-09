c79c089bb177df30ea938038358d177dd4cc40dd
Hossam <hossamabdeen17@gmail.com>
2021-05-09 16:42:21 +0200

    testing & refactoring for packets manager

--- log_analyzer/src/controllers/packets_manager.py
+++ log_analyzer/src/controllers/packets_manager.py
@@ -3,11 +3,22 @@ from src.packet_decoder import PacketDecoder
 
 class PacketsManager(object):
     class __PacketsManager(object):
+        """ Managing the packets by using the PacketDecoder to decode them and
+        assign ids for them and for their events
+        """
+
         def __init__(self):
             self.__latest_packet = None
             self.__packets_count = 1
 
         def add(self, packet):
+            """Decode a packet from the profiler and store in __latest_packet
+            variable
+
+            Args:
+                packet: Bytes to be decoded using the  PacketDecoder
+            """
+
             packet = PacketDecoder(packet).decode_packet()
             packet.set_id(self.__packets_count)
             packet.set_id_for_events()
@@ -15,10 +26,22 @@ class PacketsManager(object):
             self.__packets_count += 1
 
         def get_latest_packet(self):
+            """A getter for the last packet added to the mananger
+
+            Returns:
+                A packet object
+            """
+
             return self.__latest_packet
 
         def get_packet_count(self):
-            return self.__packets_count
+            """A getter for the count of the packets arrived
+
+            Returns:
+                An integer represents the count of the packets
+            """
+
+            return self.__packets_count - 1
 
     __instance = None
 
